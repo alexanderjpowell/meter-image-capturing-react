ae633d3356885ae8b5084df3962a00bf46cbc99d
Alexander Powell <ajpowell@email.wm.edu>
2020-05-11 16:17:15 -0700

    deployed to firebase

--- src/views/UserList/components/UsersTable/UsersTable.js
+++ src/views/UserList/components/UsersTable/UsersTable.js
@@ -6,21 +6,16 @@ export default function UsersTable() {
     columns: [
       { title: 'Name', field: 'name' },
       { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-      {
+      { title: 'Pin Code', field: 'pinCode' },
+      /*{
         title: 'Birth Place',
         field: 'birthCity',
         lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-      },
+      },*/
     ],
     data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
-      },
+      { name: 'test1', surname: 'test1', pinCode: '****' },
+      { name: 'test2', surname: 'test2', pinCode: '****' },
     ],
   });
 
